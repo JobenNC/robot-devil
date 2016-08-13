8a091d056af56f84d42cb325ac77dadc05e4dc39
root <root@localhost.localdomain>
2016-08-13 19:52:51 -0400

    better

--- robot-devil-broke.py
+++ robot-devil-broke.py
@@ -1,4 +1,5 @@
 import asyncio, discord, logging, pdb
+from yarnSpin import yarnSpin
 
 logging.basicConfig(level=logging.DEBUG)
 client = discord.Client()
@@ -13,18 +14,28 @@ def hailSatan():
     print(connect)
     print('probably done')
 
-@client.async_event
+#@client.async_event
 #def on_message(message):
 #    if message.content.startswith('$greet'):
 #        yield from client.send_message(message.channel, 'Say hello')
 #        msg = yield from client.wait_for_message(author=message.author, content='hello')
 #        yield from client.send_message(message.channel, 'Hello.')
 
+@client.async_event
 def on_message(message):
-    if message.content.startswith('$test'):
+    if message.content.startswith('Storytime'):
         #yield from client.send_message(message.channel, 'Say hello')
         #msg = yield from client.wait_for_message(author=message.author, content='hello')
-        yield from client.send_message(message.channel, 'Test Message.')
+        yield from client.send_message(message.channel, 'Story Time.')
+        members = client.get_all_members()
+        members = set(member for member in members)
+        for member in members:
+            yield from client.send_message(message.channel, member.name)
+
+        story = yarnSpin.findYarn([member.name for member in members])
+        #TODO: 2,000 char limit. split up stories
+        yield from client.send_message(message.channel, story[0])
+        yield from client.send_message(message.channel, story[1])
 
 
 loop = asyncio.get_event_loop()
